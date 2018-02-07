b239d1b271b27e37a940040e7b9dfdfee06d4b8a
Aidan <Aidan@qq.com>
2018-02-07 18:55:40 +0800

    update settings

--- settings.py
+++ settings.py
@@ -1,50 +1,28 @@
 settings={
-	# 'office':{
-	# 	'url':'http://www.pspme.cn/',
-	# 	'account':'',
-	#     'password':''
-	# },
-	'gloryskyM':{
-		'url':'https://glorysky.pspme.cn/manager/',
-		'account':'1016657151@qq.com',
-	    'password':'litie123'
+	'office':{
+		'url':'############',
+		'account':'############',
+	    'password':'############'
 	},
-	# 'gloryskyW':{
-	# 	'url':'https://glorysky.pspme.cn/web',
-	# 	'account':'100001000100006',
-	#     'password':'abc123'
-	# },
-	# 'psdemoM':{
-	# 	'url':'https://psdemo.pspme.cn/manager',
-	# 	'account':'014000000000000',
-	#     'password':'abc123'
-	# },
-	# 'psdemoW':{
-	# 	'url':'https://psdemo.pspme.cn/web',
-	# 	'account':'abc123@qq.com',
-	#     'password':'abc123'
-	# }
+	'gloryskyM':{
+		'url':'############',
+		'account':'############',
+	    'password':'############'
+	},
+	'gloryskyW':{
+		'url':'############',
+		'account':'############',
+	    'password':'############'
+	},
+	'psdemoM':{
+		'url':'############',
+		'account':'############',
+	    'password':'############'
+	},
+	'psdemoW':{
+		'url':'############',
+		'account':'############',
+	    'password':'############'
+	}
 }
 
-
-# gloryskyM={
-# 	'account':'1016657151@qq.com',
-# 	'password':'litie123'
-# }
-
-# gloryskyW={
-# 	'account':'100001000100006',
-# 	'password':'abc123'
-# }
-
-# psdemoM={
-# 	'account':'014000000000000',
-# 	'password':'abc123'
-# }
-
-# psdemoW={
-# 	'account':'abc123@qq.com',
-# 	'password':'abc123'
-# }
-
-# officeURL="http://www.pspme.cn/"
